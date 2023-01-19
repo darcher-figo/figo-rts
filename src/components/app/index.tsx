7992cac7e7ba33dc3a8cbd61589677e804d044bd
Dylan Archer <darcher@insurefigo.com>
2023-01-19 13:10:20 -0500

    updates to table styling

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,11 +1,9 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable unicorn/prefer-add-event-listener */
 import * as React from 'react';
-import data, { title } from './data.fixture';
+import { title } from './data.fixture';
 import worker from '../../workers';
 import ReactDataGrid from '@inovua/reactdatagrid-community';
 import '@inovua/reactdatagrid-community/index.css';
+import '@inovua/reactdatagrid-community/base.css';
 import './style.scss';
 
 interface ProductT {
@@ -30,7 +28,7 @@ interface RequestT {
   loading?: boolean;
 }
 
-export const App: React.FC<{ heading?: string }> = ({ heading = title }): JSX.Element => {
+export const App: React.FC = (): JSX.Element => {
   const [state, setState] = React.useState<RequestT>({
     limit: 0,
     skip: 0,
@@ -42,23 +40,6 @@ export const App: React.FC<{ heading?: string }> = ({ heading = title }): JSX.El
   // eslint-disable-next-line react-hooks/exhaustive-deps
   let webWorker: Worker | null = new Worker(worker);
 
-  // Let's make this intentionally slow, in order to see the loading better
-  // const mockTimeConsumingLoad = React.useCallback(() => {
-  //   const data = new Promise(resolve => {
-  //     setTimeout(() => {
-  //       resolve([
-  //         { name: 'John Grayner', age: 35, id: 0 },
-  //         { name: 'Mary Stones', age: 25, id: 1 },
-  //         { name: 'Robert Fil', age: 27, id: 2 },
-  //         { name: 'Bob Margin', age: 17, id: 3 },
-  //         { name: 'Hillary Wilson', age: 53, id: 4 },
-  //         { name: 'Angela Berkley', age: 44, id: 5 },
-  //         { name: 'Franklin Richardson', age: 37, id: 6 },
-  //       ]);
-  //     }, 1000);
-  //   });
-  // });
-
   function swapLoadingTo(loading: boolean | undefined): void {
     setState(options => ({ ...options, loading }));
   }
@@ -69,6 +50,7 @@ export const App: React.FC<{ heading?: string }> = ({ heading = title }): JSX.El
 
   React.useEffect((): (() => void) => {
     if (webWorker) {
+      // eslint-disable-next-line unicorn/prefer-add-event-listener, react-hooks/rules-of-hooks
       webWorker.onmessage = (event: MessageEvent<string>) => {
         if (event.data === 'BAD') {
           return;
@@ -78,6 +60,7 @@ export const App: React.FC<{ heading?: string }> = ({ heading = title }): JSX.El
         setState(options => ({ ...options, ...JSON.parse(event.data) }));
       };
 
+      // eslint-disable-next-line unicorn/prefer-add-event-listener
       webWorker.onerror = () => {
         console.error('Error');
       };
@@ -97,19 +80,27 @@ export const App: React.FC<{ heading?: string }> = ({ heading = title }): JSX.El
 
   return (
     <>
-      <h1 className="app-title">{heading}</h1>
+      <h1 className="app-title">
+        Figo<span className="title-abbr">RTS</span>
+      </h1>
 
       <div style={{ maxWidth: '100%' }}>
         <ReactDataGrid
           idProperty="id"
           columns={[
-            { name: 'title', header: 'Title', minWidth: 50, defaultFlex: 2 },
-            { name: 'description', header: 'Summary', maxWidth: 400, defaultFlex: 1 },
+            { name: 'title', header: 'Title', minWidth: 50, defaultFlex: 1.5 },
+            {
+              name: 'description',
+              header: 'Summary',
+              defaultFlex: 3,
+              minWidth: 480,
+              maxWidth: 640,
+            },
             {
               name: 'price',
               header: 'Price',
               minWidth: 50,
-              defaultFlex: 2,
+              defaultFlex: 1,
               type: 'number',
               render: ({ value }: { value: number }) =>
                 value.toLocaleString('en-US', { style: 'currency', currency: 'USD' }),
@@ -118,7 +109,7 @@ export const App: React.FC<{ heading?: string }> = ({ heading = title }): JSX.El
               name: 'discountPercentage',
               header: 'Discount',
               minWidth: 50,
-              defaultFlex: 1,
+              defaultFlex: 0.75,
               type: 'number',
               render: ({ value }: { value: number }) =>
                 (value / 100).toLocaleString('en', { style: 'percent' }),
@@ -127,16 +118,21 @@ export const App: React.FC<{ heading?: string }> = ({ heading = title }): JSX.El
               name: 'rating',
               header: 'Rating',
               minWidth: 50,
-              defaultFlex: 1,
+              defaultFlex: 0.75,
               type: 'number',
               render: ({ value }: { value: number }) => `${value}/5`,
             },
-            { name: 'stock', header: 'Stock', minWidth: 50, defaultFlex: 1, type: 'number' },
-            { name: 'brand', header: 'Brand', minWidth: 150, defaultFlex: 2 },
-            { name: 'category', header: 'Category', minWidth: 150, defaultFlex: 2 },
+            { name: 'stock', header: 'Stock', minWidth: 50, defaultFlex: 0.75, type: 'number' },
+            { name: 'brand', header: 'Brand', minWidth: 150, defaultFlex: 1 },
+            { name: 'category', header: 'Category', minWidth: 150, defaultFlex: 1 },
           ]}
+          theme={'default-light'}
           dataSource={[...state.products]}
-          style={{ minHeight: 550 }}
+          style={{ minHeight: 800 }}
+          minRowHeight={36}
+          rowHeight={48}
+          enableColumnAutosize={true}
+          pageSizes={[25, 50, 100, 250, 500, 1000]}
         />
       </div>
     </>
